6e35068cb11b4cfaee6ceaf96d9414e9372a7532
JeyZeta <jey_zeta@hotmail.com>
2016-10-30 13:43:26 -0500

    Update Dangerous.py

--- Dangerous/Dangerous.py
+++ Dangerous/Dangerous.py
@@ -5,6 +5,13 @@ import os
 import webbrowser
 import platform
 import subprocess
+import Cryptool.cryptool
+import ftplib
+import ConfigParser
+import urllib
+import gzip
+import csv
+
 
 def limpiar():
 	if os.name == "nt":
@@ -30,11 +37,10 @@ def menu():
 
     print '''
 
-\033[1;32m 1. Index             \033[1;32m 4. IpScanner         \033[1;32m 7. Sqlmap         \033[1;32m 10. Hash
-
-\033[1;32m 2. GoSFinder         \033[1;32m 5. TheHarvester      \033[1;32m 8. Weevely        \033[1;32m 11. DDoS
-
-\033[1;32m 3. Dorkscan          \033[1;32m 6. Golismero         \033[1;32m 9. Cifrado        \033[1;32m 12. Extra
+  \033[1;32m 1. Footprinting      \033[1;32m 5. IpScanner         \033[1;32m  9. Metadatos     \033[1;32m 13. Hash
+  \033[1;32m 2. Fingerprinting    \033[1;32m 6. BruteForce        \033[1;32m 10. DDoS          \033[1;32m 14. Sqli
+  \033[1;32m 3. Doxing            \033[1;32m 7. Racp              \033[1;32m 11. Backdoor      \033[1;32m 15. About
+  \033[1;32m 4. Dorkscan          \033[1;32m 8. GoSFinder         \033[1;32m 12. Cryptool      \033[1;32m 16. Exit 
 
     '''
     while True:
@@ -42,31 +48,36 @@ def menu():
        d = raw_input("\033[1;36mDangerous > \033[1;m")
 
        if d == "1":
-           subprocess.call(['python','Index/index.py'])
+           subprocess.call(['python','footprinting/foot.py'])
        if d == "2":
-           subprocess.call(['python','GoSFinder-master/GoSFinder.py'])
+           subprocess.call(['python','fingerprinting/finger.py'])
        if d == "3":
-           subprocess.call(['python','Dorkscan/Dorkscan.py'])
+           subprocess.call(['python','dox/doxing.py'])
        if d == "4":
-           subprocess.call(['python','IpScanner/IpScanner.py'])
+           subprocess.call(['python','Dorkscan/Dorkscan.py'])
        if d == "5":
-           subprocess.call(['python','TheHarvester/theHarvester.py'])
+           subprocess.call(['python','IpScanner/IpScanner.py'])
        if d == "6":
-           subprocess.call(['python','Golismero/golismero.py'])
+           subprocess.call(['python','cupp/cupp.py'])
        if d == "7":
-           subprocess.call(['python','sqlmap/sqlmap.py'])
+           subprocess.call(['perl','RACP-master/RACP.pl'])
        if d == "8":
-           subprocess.call(['python','Weevely/weevely.py'])
+           subprocess.call(['python','GoSFinder/GoSFinder.py'])
        if d == "9":
-           subprocess.call(['python','Cifrado/Cifrado.py'])
+           subprocess.call(['python','Metadatos/metadatos.py'])
        if d == "10":
-           subprocess.call(['python','Hash/Hash.py'])
+           subprocess.call(['python','ddos/ddos.py'])
        if d == "11":
-           url = raw_input("Url a la que quieres atacar.. :")
-           subprocess.call(['perl','ddos/slowloris.pl','-dns',url])
+           subprocess.call(['python','weevely/weevely.py'])
        if d == "12":
-           subprocess.call(['python','Calculadora/Calculadora.py'])
-       if d == "exit":
+           subprocess.call(['python','criptografia/s.py'])
+       if d == "13":
+           subprocess.call(['python','Hash/Hash.py'])
+       if d == "14":
+           subprocess.call(['python','sqli/sql.py'])
+       if d == "15":
+           webbrowser.open('https://www.facebook.com/JeyZetaCyberWar/')
+       if d == "16":
            sys.exit()
 
 menu()
